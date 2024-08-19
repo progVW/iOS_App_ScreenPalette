4869a91b6d99bb540b26f763fb477405d0af7cf2
SukharevS <suharev.s@icloud.com>
2024-08-19 20:56:18 +0300

    Initial Commit

---
+++ Screen_Color_Palette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Screen_Color_Palette
+//
+//  Created by Sergey Sukharev on 19.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
