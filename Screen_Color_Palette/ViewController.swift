052612e4976854cdaff004b68b06c7f4f10ec701
Sergey Sukharev <zematut@Sergeys-MacBook-Air.local>
2024-08-25 13:35:04 +0300

    Added change color from slider.value

--- Screen_Color_Palette/ViewController.swift
+++ Screen_Color_Palette/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var minButton: UIButton!
+    
+    @IBOutlet weak var mediumButton: UIButton!
+    
+    @IBOutlet weak var maxButton: UIButton!
+    
+    @IBOutlet weak var sliderRedOUT: UISlider!
+    
+    @IBOutlet weak var sliderGreenOUT: UISlider!
+    
+    @IBOutlet weak var sliderBlueOUT: UISlider!
+    
+    @IBOutlet weak var picture: UIView!
+    
+    @IBOutlet weak var labelValueRed: UILabel!
+    
+    @IBOutlet weak var labelValueGreen: UILabel!
+    
+    @IBOutlet weak var labelValueBlue: UILabel!
+    
+    func mediana(_ sender: UISlider) {
+        sender.value = (sender.maximumValue - sender.minimumValue) / 2 + sender.minimumValue
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mediana(sliderRedOUT)
+        mediana(sliderBlueOUT)
+        mediana(sliderGreenOUT)
+        labelValueRed.text = "\(Int(sliderRedOUT.value))"
+        labelValueGreen.text = "\(Int(sliderGreenOUT.value))"
+        labelValueBlue.text = "\(Int(sliderBlueOUT.value))"
     }
 
-
+    @IBAction func sliderRed(_ sender: UISlider) {
+        labelValueRed.text = "\(Int(sender.value))"
+        picture.backgroundColor = UIColor.init(red: (CGFloat(sender.value) / 255.0), green: (CGFloat(sliderGreenOUT.value) / 255.0), blue: (CGFloat(sliderBlueOUT.value) / 255.0), alpha: 1)
+    }
+    
+    @IBAction func sliderGreen(_ sender: UISlider) {
+        labelValueGreen.text = "\(Int(sender.value))"
+        picture.backgroundColor = UIColor.init(red: (CGFloat(sliderRedOUT.value) / 255.0), green: (CGFloat(sender.value) / 255.0), blue: (CGFloat(sliderBlueOUT.value) / 255.0), alpha: 1)
+    }
+    
+    @IBAction func sliderBlue(_ sender: UISlider) {
+        labelValueBlue.text = "\(Int(sender.value))"
+        picture.backgroundColor = UIColor.init(red: (CGFloat(sliderRedOUT.value) / 255.0), green: (CGFloat(sliderBlueOUT.value) / 255.0), blue: (CGFloat(sender.value) / 255.0), alpha: 1)
+    }
 }
 
